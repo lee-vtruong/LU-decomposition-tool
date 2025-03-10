6e7381f63a82a55c962a600025e7673f8824cc45
leev.truong <letruong2005ooo@gmail.com>
2025-03-10 07:35:55 +0700

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'LU DECOMPOSITION',
+  description: 'Created by leev.truong',
+  generator: 'leev.truong',
 }
 
 export default function RootLayout({
